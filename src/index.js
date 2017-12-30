f308e111a9c43449f589e4a91934fd40c90a9c36
lihongye <1121807592@qq.com>
2017-12-30 13:23:03 +0800

    添加tab

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/onsenui/css/onsenui.css'
-// import './css/onsen-css-components.css'
-// import './css/couponStyle.css'
+import '../node_modules/onsenui/css/onsenui.css';
+import '../node_modules/onsenui/css/onsen-css-components.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
